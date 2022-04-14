e29be62ab9a156c621677cab578b4dbd9efda47f
cikavladimir <cikavladimir@gmail.com>
2022-04-14 17:42:36 -0400

    Add files via upload

---
+++ TaxonClsType.sql
@@ -0,0 +1,6 @@
+INSERT INTO TaxonClsType  (id, code, name)  values  (10001, "EU-ID", "Entity Unit");
+INSERT INTO TaxonClsType  (id, code, name)  values  (10002, "OU-ID", "Org Unit");
+INSERT INTO TaxonClsType  (id, code, name)  values  (10003, "AU-ID", "Asset Unit");
+INSERT INTO TaxonClsType  (id, code, name)  values  (10004, "AT-ID", "Asset Type");
+INSERT INTO TaxonClsType  (id, code, name)  values  (10005, "CU-ID", "Component Unit");
+INSERT INTO TaxonClsType  (id, code, name)  values  (10006, "G-ID", "Global");
